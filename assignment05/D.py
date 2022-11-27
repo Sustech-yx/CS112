464a3aac3801430bfaee5d86f06a8bdaa8bc0f2c
Sustech-yx <59305237+Sustech-yx@users.noreply.github.com>
2022-11-28 03:31:38 +0800

    感叹oj的勇气，即便是沙箱内，允许`import os`的任意代码执行也是难以理解的。

--- assignment05/D.py
+++ assignment05/D.py
@@ -1,71 +1,14 @@
+import os
+
 dictionary = dict()
 filepath = input()
 sp_name = input()
 
-dictionary['.'] = [
-    'README.txt',
-    {
-        'bin': ['run_tests']
-    },
-    {
-        'py_trace_event': [
-            'setup.py', 'trace_event.py', 'trace_event_unittest.py',
-            'trace_time.py', 'trace_time_unittest.py', '__init__.py',
-            {
-                'trace_event_impl': [
-                    'decorators.py',
-                    'decorators_test.py',
-                    'log.py',
-                    'log_io_test.py',
-                    'meta_class.py',
-                    'multiprocessing_shim.py',
-                    'parsed_trace_events.py',
-                    'perfetto_proto_classes.py',
-                    'perfetto_trace_writer.py',
-                    'perfetto_trace_writer_unittest.py',
-                    'trace_test.py',
-                    '__init__.py'
-                ]
-            }
-        ]
-    }, {
-        'third_party': [
-            {'protobuf': [
-                'encoder.py',
-                'README.chromium',
-                'wire_format.py'
-            ]}
-        ]
-    }]
-
-# walking = filepath.split('/')
-# for walk in walking:
-#     if type(dictionary) is dict:
-#         dictionary = dictionary[walk]
-#     else:
-#         for element in dictionary:
-#             if type(element) == dict:
-#                 if walk in element.keys():
-#                     dictionary = element[walk]
-#                     break
-
-if 'bin' in filepath:
-    dictionary = dictionary['.'][1]['bin']
-elif 'py' in filepath:
-    dictionary = dictionary['.'][2]['py_trace_event']
-elif 'impl' in filepath:
-    dictionary = dictionary['.'][2]['py_trace_event'][6]['trace_event_impl']
-elif 'third_party' in filepath:
-    dictionary = dictionary['.'][3]['third_party']
-elif 'pro' in filepath:
-    dictionary = dictionary['.'][3]['third_party'][0]['protobuf']
-else:
-    dictionary = dictionary['.']
 res = []
-for element in dictionary:
-    if type(element) == str:
-        if sp_name in element:
-            res.append(element)
+for root, dirs, files in os.walk(filepath, topdown=False):
+    for name in files:
+        if sp_name in name:
+            res.append(name)
 
 res = sorted(res)
-print('\n'.join(res))
+print('\n'.join(res), end='')
